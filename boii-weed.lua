c53836925c7e956dfe48d4f9421076a2c00a0ab0
WoolerS0912 <136260463+WoolerS0912@users.noreply.github.com>
2024-03-06 23:44:38 +0200

    Update boii-weed.lua
    123

--- boii-weed.lua
+++ boii-weed.lua
@@ -4,74 +4,74 @@
 
 Language = {
     Debug = {
-        ['targetmissingdata'] = 'Someone targeted a plant and the data could not be found!' -- Debug server side print
+        ['targetmissingdata'] = 'Kažkas taikėsi į augalą ir nepavyko rasti duomenų!' -- Debug server side print
     },
     XP = {
-        ['commandtext'] = 'Displays your current drug manufacturing xp.', -- Command text
-        ['xplevelnotif'] = 'Drug Manufacturing:', -- Notification
+        ['commandtext'] = 'Rodo jūsų dabartinę narkotikų gamybos xp.', -- Command text
+        ['xplevelnotif'] = 'Narkotikų gamyba:', -- Notification
     },
     Targeting = {
-        ['plantsicon'] = 'fa-solid fa-cannabis', -- Icon used by qb-target for hydrochloric location visit; https://fontawesome.com to change
-        ['plantslabel'] = 'Check Plant', -- Label used by qb-target for hydrochloric location
+        ['plantsicon'] = 'fa-kietosios fa-kanapės', -- Icon used by qb-target for hydrochloric location visit; https://fontawesome.com to change
+        ['plantslabel'] = 'Patikrinkite Augalą', -- Label used by qb-target for hydrochloric location
     },
     Planting = {
-        ['tooclose'] = 'You are too close to another plant..', -- Notification
-        ['wrongsoil'] = 'You cant plant here look for some more fertile land..', -- Notification
-        ['plantedseednoxp'] = 'You planted a seed successfully!', -- Notification
-        ['plantedseedxp'] = 'You successfully planted a seed and earned some xp!', -- Notification
-        ['notrowel'] = 'You cant plant seeds without a '..Config.Plants.Outdoor.Actions.Plant.Required.label..'..', -- Notification
-        ['nofertilizer'] = 'You cant feed plants without '..Config.Plants.Outdoor.Actions.Feed.Required.label..'..', -- Notification
-        ['nowater'] = 'You cant water plants without '..Config.Plants.Outdoor.Actions.Water.Required.label..'..', -- Notification
-        ['noshears'] = 'You cant trim plants without '..Config.Plants.Outdoor.Actions.Harvest.Required.label..'..', -- Notification
-        ['harvestxp'] = 'You successfully harvested a plant and earned some xp!', -- Notification
-        ['harvestnoxp'] = 'You successfully harvested a plant!', -- Notification
-        ['destroyedplant'] = 'You successfully destroyed a plant!', -- Notification
-        ['harvestplant'] = 'Harvesting Plant..', -- Progressbar
-        ['destroyplant'] = 'Destroying Plant..', -- Progressbar
-        ['feedplant'] = 'Feeding Plant..', -- Progressbar
-        ['waterplant'] = 'Watering Plant..', -- Progressbar
-        ['plantseed'] = 'Planting Seed..', -- Progressbar
+        ['tooclose'] = 'Jūs esate per arti kito augalo..', -- Notification
+        ['wrongsoil'] = 'Negalite čia sodinti, ieškoti derlingesnės žemės..', -- Notification
+        ['plantedseednoxp'] = 'Sėkmingai pasėjote sėklą!', -- Notification
+        ['plantedseedxp'] = 'Sėkmingai pasėjote sėklą ir uždirbote šiek tiek XP!', -- Notification
+        ['notrowel'] = 'Negalite sėti sėklų be '..Config.Plants.Outdoor.Actions.Plant.Required.label..'..', -- Notification
+        ['nofertilizer'] = 'Jūs negalite patrešti augalų be '..Config.Plants.Outdoor.Actions.Feed.Required.label..'..', -- Notification
+        ['nowater'] = 'Jūs negalite laistyti augalų be '..Config.Plants.Outdoor.Actions.Water.Required.label..'..', -- Notification
+        ['noshears'] = 'Jūs negalite apkarpyti augalų be '..Config.Plants.Outdoor.Actions.Harvest.Required.label..'..', -- Notification
+        ['harvestxp'] = 'Jūs sėkmingai nuskynėte augalą ir uždirbote šiek tiek XP!', -- Notification
+        ['harvestnoxp'] = 'Jūs sėkmingai nuskynėte augalą!', -- Notification
+        ['destroyedplant'] = 'Jūs sėkmingai sunaikinote augalą!', -- Notification
+        ['harvestplant'] = 'Derliaus nuėmimo augalas..', -- Progressbar
+        ['destroyplant'] = 'Sunaikina augalą..', -- Progressbar
+        ['feedplant'] = 'Trešiamas augalas..', -- Progressbar
+        ['waterplant'] = 'Laistomas augalas..', -- Progressbar
+        ['plantseed'] = 'Sėti sėklas..', -- Progressbar
     },
     Trimming = {
-        ['noshears'] = 'You cant trim plants without a pair of '..Config.Plants.Outdoor.Actions.Trimming.Required['1'].label..'..', -- Notification
-        ['noscales'] = 'You cant trim plants without a set of '..Config.Plants.Outdoor.Actions.Trimming.Required['2'].label..'..', -- Notification
-        ['enoughbags'] = 'You dont have enough '..Config.Plants.Outdoor.Actions.Trimming.Required['3'].label..'..', -- Notification
-        ['nobags'] = 'You dont have any '..Config.Plants.Outdoor.Actions.Trimming.Required['3'].label..'..', -- Notification
-        ['trimxp'] = 'You successfully trimmed some weed and earned some xp!', -- Notification
-        ['trimnoxp'] = 'You successfully trimmed some weed!', -- Notification
-        ['trimweed'] = 'Trimming Weed..', -- Progressbar
+        ['noshears'] = 'Negalite apkarpyti augalų nesuporavęs '..Config.Plants.Outdoor.Actions.Trimming.Required['1'].label..'..', -- Notification
+        ['noscales'] = 'Negalite apkarpyti augalų be rinkinio '..Config.Plants.Outdoor.Actions.Trimming.Required['2'].label..'..', -- Notification
+        ['enoughbags'] = 'Jūs neturite pakankamai '..Config.Plants.Outdoor.Actions.Trimming.Required['3'].label..'..', -- Notification
+        ['nobags'] = 'Jūs neturite jokių '..Config.Plants.Outdoor.Actions.Trimming.Required['3'].label..'..', -- Notification
+        ['trimxp'] = 'Sėkmingai apkarpėte žolę ir uždirbote XP!', -- Notification
+        ['trimnoxp'] = 'Sėkmingai apkarpėte žolę!', -- Notification
+        ['trimweed'] = 'Žolės skynimas..', -- Progressbar
     },
     Rolling = {
-        ['nogrinder'] = 'You cant roll joints without a '..Config.Plants.Outdoor.Actions.Rolling.Required['1'].label..'..', -- Notification
-        ['nopapers'] = 'You cant roll joints without '..Config.Plants.Outdoor.Actions.Rolling.Required['2'].label..'..', -- Notification
-        ['enoughpapers'] = 'You dont have enough '..Config.Plants.Outdoor.Actions.Rolling.Required['2'].label..'..', -- Notification
-        ['rollxp'] = 'You successfully rolled some joints and earned some xp!', -- Notification
-        ['norollxp'] = 'You successfully rolled some joints!', -- Notification
-        ['rolljoint'] = 'Rolling Joints..', -- Progressbar
+        ['nogrinder'] = 'Jūs negalite sukti suktinių be '..Config.Plants.Outdoor.Actions.Rolling.Required['1'].label..'..', -- Notification
+        ['nopapers'] = 'Jūs negalite sukti suktinių be '..Config.Plants.Outdoor.Actions.Rolling.Required['2'].label..'..', -- Notification
+        ['enoughpapers'] = 'Jūs neturite pakankamai '..Config.Plants.Outdoor.Actions.Rolling.Required['2'].label..'..', -- Notification
+        ['rollxp'] = 'Sėkmingai susukote keletą suktinių ir uždirbote šiek tiek xp!', -- Notification
+        ['norollxp'] = 'Sėkmingai susukote keletą suktinių!', -- Notification
+        ['rolljoint'] = 'Sukate suktinę..', -- Progressbar
     },
     Smoking = {
-        ['nolighter'] = 'You cant smoke without a '..Config.Plants.Outdoor.Actions.Smoking.Required.label..'..', -- Notification
-        ['smokedjoint'] = 'You smoked a joint!', -- Notification
-        ['smokejoint'] = 'Hitting Blunt..', -- Progressbar
+        ['nolighter'] = 'Jūs negalite rūkyti be '..Config.Plants.Outdoor.Actions.Smoking.Required.label..'..', -- Notification
+        ['smokedjoint'] = 'Jūs rūkote suktinę!', -- Notification
+        ['smokejoint'] = 'Hitina Bluntas..', -- Progressbar
     },
     Stores = {
         -- Main
         ['smokeshopheader'] = 'Smoke On The Water', -- Menu header   
-        ['smokeshopheadertext'] = 'Buy your smoke gear or sell us some stuff!', -- Menu header
-        ['smokeshopheadericon'] = 'fa-solid fa-shop', -- Store header
+        ['smokeshopheadertext'] = 'Įsigykite rūkymo įrangą arba parduokite mums ką nors!', -- Menu header
+        ['smokeshopheadericon'] = 'fa-solid fa-parduotuvė', -- Store header
         -- Sell
-        ['smokeshopsellheader'] = 'Sell To Store!', -- Menu header   
-        ['smokeshopsellheadertext'] = 'Here you can sell your bags for cash!', -- Menu header
-        ['smokeshopsellheadericon'] = 'fas fa-cannabis', -- Store header
-        ['nobags'] = 'You dont have any bags to sell..', -- Notification
+        ['smokeshopsellheader'] = 'Parduoti į Parduotuvę!', -- Menu header   
+        ['smokeshopsellheadertext'] = 'Čia galite parduoti savo krepšelius už grynuosius pinigus!', -- Menu header
+        ['smokeshopsellheadericon'] = 'fas fa-kanapės', -- Store header
+        ['nobags'] = 'Neturite nė vieno krepšelio parduoti..', -- Notification
         -- Browse
-        ['smokeshopbrowseheader'] = 'Browse The Store!', -- Menu header   
-        ['smokeshopbrowseheadertext'] = 'Here you can purchase smoking equipment!', -- Menu header
-        ['smokeshopbrowseheadericon'] = 'fa-solid fa-basket-shopping', -- Store header
+        ['smokeshopbrowseheader'] = 'Naršykite parduotuvėje!', -- Menu header   
+        ['smokeshopbrowseheadertext'] = 'Čia galite įsigyti rūkymo įrangos!', -- Menu header
+        ['smokeshopbrowseheadericon'] = 'fa-solid fa Pirkinių krepšelis', -- Store header
         -- Timer
-        ['timer'] = 'Dude my shift hasnt started yet. Come back between '..Config.Stores.Weed.SmokeShop.StartTime..' and '..Config.Stores.Weed.SmokeShop.EndTime..'..', -- Notification
+        ['timer'] = 'Drauge, mano pamaina dar neprasidėjo. Grįžk tarp '..Config.Stores.Weed.SmokeShop.StartTime..' ir '..Config.Stores.Weed.SmokeShop.EndTime..'..', -- Notification
         -- Strains
-        ['strainheadericon'] = 'fas fa-cannabis', -- Strain header icon
+        ['strainheadericon'] = 'fas fa-kanapės', -- Strain header icon
         ['strain1header'] = 'Skunk', -- Store header
         ['strain1headertext'] = '$'..Config.Stores.Weed.SmokeShop.Drugs.Bags['weed_skunk']..' per 2g baggy.', -- Store header text
         ['strain2header'] = 'OG-Kush', -- Store header
@@ -90,12 +90,12 @@ Language = {
         ['strain8headertext'] = '$'..Config.Stores.Weed.SmokeShop.Drugs.Bags['weed_zkittlez']..' per 2g baggy.', -- Store header text
     },
     Shared = {
-        ['returnmenu'] = 'Return', -- Menu header
-		['returnmenuicon'] = 'fa-solid fa-backward', -- Menu header icon
-        ['exitmenu'] = 'Exit', -- Menu header
-		['exitmenuicon'] = 'fa-solid fa-circle-xmark', -- Menu header icon
-        ['cancelled'] = 'Action cancelled..', -- Notification
-        ['noinvent'] = 'Looks like your pockets are full..', -- Notification
-        ['outrange'] = 'Looks like you are too far away..', -- Notification  
+        ['returnmenu'] = 'Grįžti', -- Menu header
+		['returnmenuicon'] = 'fa-solid fa-atgal', -- Menu header icon
+        ['exitmenu'] = 'Išeiti', -- Menu header
+		['exitmenuicon'] = 'fa-solid fa-apskritimas-xmark', -- Menu header icon
+        ['cancelled'] = 'Veiksmas atšauktas..', -- Notification
+        ['noinvent'] = 'Panašu, kad tavo kišenės pilnos..', -- Notification
+        ['outrange'] = 'Atrodo, kad esi per toli..', -- Notification  
     }
 }
