975ec0f1e77614a99a8b2cf128d94b317274d07a
WoolerS0912 <136260463+WoolerS0912@users.noreply.github.com>
2024-03-06 23:05:47 +0200

    Update boii-drugsales.lua
    123

--- boii-drugsales.lua
+++ boii-drugsales.lua
@@ -4,84 +4,84 @@
 
 Language = {
     Debug = {
-        ['stealdrugs'] = 'had some drugs stolen whilst street selling!', -- Steal drugs debug
-        ['selldrugs'] = 'sold some drugs through street selling!', -- Sale drugs debug
-        ['deliverdrugs'] = 'sold some drugs through deliveries!', -- Delivery debug
+        ['stealdrugs'] = 'iš jo buvo pavogti narkotikai, kai prekiavo gatvėje!', -- Steal drugs debug
+        ['selldrugs'] = 'pardavė kai kuriuos narkotikus per pardavimą gatvėje!', -- Sale drugs debug
+        ['deliverdrugs'] = 'pardavė kai kuriuos narkotikus per pristatymus!', -- Delivery debug
     },
     Targeting = {
-        ['offerdrugsicon'] = 'fa-solid fa-comment', -- Icon used by qb-target for offering drugs to ped visit; https://fontawesome.com to change
-        ['offerdrugslabel'] = 'Offer Drugs', -- Label used by qb-target for offering drugs
+        ['offerdrugsicon'] = 'fa-tvirtas fa-komentaras', -- Icon used by qb-target for offering drugs to ped visit; https://fontawesome.com to change
+        ['offerdrugslabel'] = 'Siūlyti narkotikus', -- Label used by qb-target for offering drugs
     },
     Alerts = {
-        ['notif'] = '10-66: Suspicious Person in Area!', -- Notification
-        ['blip'] = '10-66: Suspicious Person in Area!' -- Blip text 
+        ['notif'] = '10-66: Įtartinas asmuo rajone!', -- Notification
+        ['blip'] = '10-66: Įtartinas asmuo rajone!' -- Blip text 
     },
     DrugSales = {
         Shared = {
-            ['nodrugs'] = 'You dont have any drugs with you..', -- Notification
-            ['enoughdrugs'] = 'You dont have enough drugs to make the deal..', -- Notification
-            ['cantsell'] = 'You cant street sell and deliver drugs at the same time..', -- Notification
-            ['novehicle'] = 'You cant sell drugs from a vehicle..', -- Notification
-            ['noservice'] = 'Service jobs are not allowed to sell drugs. Respect the law!', -- Notification
-            ['noblacklist'] = 'Your job is not allowed to sell drugs..', -- Notification
+            ['nodrugs'] = 'Neturi su savimi jokių narkotikų..', -- Notification
+            ['enoughdrugs'] = 'Jūs neturite pakankamai narkotikų, kad susitartumėte..', -- Notification
+            ['cantsell'] = 'Negalite parduoti ir pristatyti narkotikų tuo pačiu metu..', -- Notification
+            ['novehicle'] = 'Jūs negalite parduoti narkotikų iš automobilio..', -- Notification
+            ['noservice'] = 'Tarnyboms draudžiama parduoti narkotikus. Gerbk įstatymus!', -- Notification
+            ['noblacklist'] = 'Jūsų darbui draudžiama parduoti narkotikus..', -- Notification
         },
         Menus = {
-            ['phoneheader'] = 'Burner Phone', -- Menu header   
-            ['phoneheadertext'] = 'Start street selling or make deliveries!', -- Menu header text
-            ['phoneheadericon'] = 'fa-solid fa-user-ninja', -- Menu header icon
-            ['streetsellingheader'] = 'Toggle Street Selling', -- Menu header  
-            ['streetsellingheadericon'] = 'fa-solid fa-cannabis', -- Menu header icon
-            ['deliveriesheader'] = 'Request Delivery', -- Menu header 
-            ['deliveriesheadericon'] = 'fa-solid fa-cannabis', -- Menu header icon
+            ['phoneheader'] = 'Skubus telefonas', -- Menu header   
+            ['phoneheadertext'] = 'Pradėkite prekybą gatvėje arba pristatykite!', -- Menu header text
+            ['phoneheadericon'] = 'fa-solid fa-vartotojas-nindzė', -- Menu header icon
+            ['streetsellingheader'] = 'Pakeisti pardavimą gatvėje', -- Menu header  
+            ['streetsellingheadericon'] = 'fa-kietosios fa-kanapės', -- Menu header icon
+            ['deliveriesheader'] = 'Pristatymo užklausa', -- Menu header 
+            ['deliveriesheadericon'] = 'fa-kietosios fa-kanapės', -- Menu header icon
         },
         Street = {
-            ['startedselling'] = 'You started selling drugs! Go speak to some locals!', -- Notification
-            ['stoppedselling'] = 'You stopped selling drugs.', -- Notification
-            ['enoughcops'] = 'Not enough on duty officers! Required: ('..Config.DrugSales.Street.Cops..')', -- Notification
-            ['timer'] = 'You can only sell between '..Config.DrugSales.Street.Times.Start..' and '..Config.DrugSales.Street.Times.End..'..', -- Notification
-            ['alreadysold'] = 'You have already tried to offer this person drugs..', -- Notification
-            ['nosteal'] = 'You didnt have any drugs they could steal..', -- Notification
-            ['enoughsteal'] = 'Your didnt have enough drugs worth stealing..' -- Notification
+            ['startedselling'] = 'Jūs pradėjote pardavinėti narkotikus! Eik pasikalbėk su vietiniais!', -- Notification
+            ['stoppedselling'] = 'Nustojote pardavinėti narkotikus.', -- Notification
+            ['enoughcops'] = 'Neužtenka budinčių pareigūnų! Būtina: ('..Config.DrugSales.Street.Cops..')', -- Notification
+            ['timer'] = 'Galite parduoti tik tarp '..Config.DrugSales.Street.Times.Start..' ir '..Config.DrugSales.Street.Times.End...'.', -- Notification
+            ['alreadysold'] = 'Jūs jau bandėte pasiūlyti šiam asmeniui narkotikų.', -- Notification
+            ['nosteal'] = 'Jūs neturėjote jokių narkotikų, kuriuos jie galėtų pavogti..', -- Notification
+            ['enoughsteal'] = 'Tu neturėjai pakankamai narkotikų, kuriuos verta pavogti..' -- Notification
         },
         Deliveries = {
-            ['startedselling'] = 'You requested a delivery! Wait for a customer!', -- Notification
-            ['stoppedselling'] = 'You stopped your delivery!', -- Notification
-            ['enoughcops'] = 'Not enough on duty officers! Required: ('..Config.DrugSales.Deliveries.Cops..')', -- Notification
-            ['timer'] = 'You can only sell between '..Config.DrugSales.Deliveries.Times.Start..' and '..Config.DrugSales.Deliveries.Times.End..'..', -- Notification
-            ['alreadysold'] = 'You have already tried to deliver to this person..', -- Notification
-            ['cooldowntimer'] = 'You can only request a delivery once every ('..Config.DrugSales.Deliveries.Cooldown..') mins..', -- Notification
-            ['customerready'] = 'A customer is waiting for their delivery!', -- Notification
-            ['nodelivery'] = 'Im not intersted in buying anything from you.. leave..', -- Notification
-            ['saleblip'] = 'Delivery Customer', -- Blip string
+            ['startedselling'] = 'Jūs paprašėte pristatymo! Laukite kliento!', -- Notification
+            ['stoppedselling'] = 'Sustabdėte pristatymą!', -- Notification
+            ['enoughcops'] = 'Neužtenka budinčių pareigūnų! Būtina: ('..Config.DrugSales.Deliveries.Cops..')', -- Notification
+            ['timer'] = 'Galite parduoti tik tarp '..Config.DrugSales.Deliveries.Times.Start..' ir '..Config.DrugSales.Deliveries.Times.End...'.', -- Notification
+            ['alreadysold'] = 'Jūs jau bandėte pristatyti šiam asmeniui..', -- Notification
+            ['cooldowntimer'] = 'Pristatymo užklausą galite pateikti tik kartą per (..Config.DrugSales.Deliveries.Cooldown..') minutes..', -- Notification
+            ['customerready'] = 'Klientas laukia savo pristatymo!', -- Notification
+            ['nodelivery'] = 'Man neįdomu nieko iš tavęs pirkti.. išeik..', -- Notification
+            ['saleblip'] = 'Pristatymo klientas', -- Blip string
         },
     },
     Stores = {
         BulkSelling = {
-            ['bulkbuyerheader'] = 'Bulk Buyer', -- Menu header   
-            ['bulkbuyerheadertext'] = 'Bulk sell your drugs here.', -- Menu header text
-            ['bulkbuyerheadericon'] = 'fa-solid fa-user-ninja', -- Menu header icon
-            ['bulkbuyerbagsheader'] = 'Bulk Sell Bags', -- Menu header   
-            ['bulkbuyerbagsheadertext'] = 'Bulk sell your baggies here!', -- Menu header text
-            ['bulkbuyerbagsheadericon'] = 'fa-solid fa-cannabis', -- Menu header icon
-            ['bulkbuyerouncesheader'] = 'Bulk Sell Ounces', -- Menu header   
-            ['bulkbuyerouncesheadertext'] = 'Bulk sell your ounces here!', -- Menu header text
-            ['bulkbuyerouncesheadericon'] = 'fa-solid fa-cannabis', -- Menu header icon
-            ['timer'] = 'You can only sell between '..Config.Stores.BulkSelling.Times.Start..' and '..Config.Stores.BulkSelling.Times.End..'..', -- Notification
+            ['bulkbuyerheader'] = 'Didelio kiekio pirkėjas', -- Menu header   
+            ['bulkbuyerheadertext'] = 'Parduokite savo narkotikus čia.', -- Menu header text
+            ['bulkbuyerheadericon'] = 'fa-solid fa-vartotojas-nindzė', -- Menu header icon
+            ['bulkbuyerbagsheader'] = 'Parduodu didelio kiekio krepšius', -- Menu header   
+            ['bulkbuyerbagsheadertext'] = 'Parduokite savo krepšius masiškai čia!', -- Menu header text
+            ['bulkbuyerbagsheadericon'] = 'fa-kietosios fa-kanapės', -- Menu header icon
+            ['bulkbuyerouncesheader'] = 'Parduokite urmu uncijomis', -- Menu header   
+            ['bulkbuyerouncesheadertext'] = 'Parduokite savo uncijas dideliais kiekiais čia!', -- Menu header text
+            ['bulkbuyerouncesheadericon'] = 'fa-kietosios fa-kanapės', -- Menu header icon
+            ['timer'] = 'Galite parduoti tik tarp '..Config.Stores.BulkSelling.Times.Start..' ir '..Config.Stores.BulkSelling.Times.End...'.', -- Notification
             BlackMarket = {
-                ['blackmarketheader'] = 'Black Market', -- Menu header   
-                ['blackmarketheadertext'] = 'Check what items are in stock here.', -- Menu header text
-                ['blackmarketheadericon'] = 'fa-solid fa-cash-register', -- Menu header icon
-                ['blackmarketstorelabel'] = 'Black Market', -- Store header
+                ['blackmarketheader'] = 'Juodoji rinka', -- Menu header   
+                ['blackmarketheadertext'] = 'Patikrinkite, kokių prekių yra sandėlyje.', -- Menu header text
+                ['blackmarketheadericon'] = 'fa-solid fa-kasos aparatas', -- Menu header icon
+                ['blackmarketstorelabel'] = 'Juodoji rinka', -- Store header
             }
         }
     },
     Shared = {
-        ['returnmenu'] = 'Return', -- Menu header
-		['returnmenuicon'] = 'fa-solid fa-backward', -- Menu header icon
-        ['exitmenu'] = 'Exit', -- Menu header
-		['exitmenuicon'] = 'fa-solid fa-circle-xmark', -- Menu header icon
-        ['cancelled'] = 'Action cancelled..', -- Notification
-        ['noinvent'] = 'Looks like your pockets are full..', -- Notification
-        ['outrange'] = 'Looks like you are too far away..' -- Notification  
+        ['returnmenu'] = 'Grįžti', -- Menu header
+		['returnmenuicon'] = 'fa-solid fa-atgal', -- Menu header icon
+        ['exitmenu'] = 'Išeiti', -- Menu header
+		['exitmenuicon'] = 'fa-kietas fa apskritimas xmark', -- Menu header icon
+        ['cancelled'] = 'Veiksmas atšauktas..', -- Notification
+        ['noinvent'] = 'Panašu, kad tavo kišenės pilnos..', -- Notification
+        ['outrange'] = 'Atrodo, kad esi per toli..' -- Notification  
     }
 }
