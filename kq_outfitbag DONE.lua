6422bd86915f5a520abda2004a468939bff3a191
Bimbam555 <162443334+Bimbam555@users.noreply.github.com>
2024-03-24 20:40:56 +0000

    Update kq_outfitbag DONE.lua

--- kq_outfitbag DONE.lua
+++ kq_outfitbag DONE.lua
@@ -3,15 +3,15 @@
 
 
 Locale = {
-    ['Open Bag'] = 'Atidarytas krepšys',
+    ['Open Bag'] = 'Atidarykite krepšys',
     ['Outfit bag'] = 'Aprangos krepšys',
     ['Pickup the bag'] = 'Paimk krepšį',
-    ['Make public'] = 'Padaryti viešą',
+    ['Make public'] = 'Padaryti visiems matomą',
     ['Make private'] = 'Padaryti privatų',
     ['~r~This outfit isn\'t suitable for you'] = '~r~Ši apranga jums netinka',
     ['~w~[~g~{OPEK_KEYBIND}~w~] to open [~g~{PICKUP_KEYBIND}~w~] to pickup the bag'] = '~w~[~g~{OPEK_KEYBIND}~w~] atidaryti [~g~{PICKUP_KEYBIND}~w~] ir pasiimti krepšį',
     ['~w~Press [~g~{PUBLIC_KEYBIND}~w~] to open make the bag ~r~private'] = '~w~Paspauskite [~g~{PUBLIC_KEYBIND}~w~], kad atidarytumėte krepšį ~r~privatų',
-    ['~w~Press [~g~{PUBLIC_KEYBIND}~w~] to open make the bag ~g~public'] = '~w~Paspauskite [~g~{PUBLIC_KEYBIND}~w~], kad atidarytumėte krepšį ~g~viešą',
+    ['~w~Press [~g~{PUBLIC_KEYBIND}~w~] to open make the bag ~g~public'] = '~w~Paspauskite [~g~{PUBLIC_KEYBIND}~w~], kad atidarytumėte krepšį ~g~visiems matomą',
     ['~w~[~g~{OPEN_KEYBIND}~w~] to open the bag'] = '~w~[~g~{OPEN_KEYBIND}~w~], kad atidarytumėte krepšį',
     ['~r~Wait a bit before you switch outfits again'] = '~r~ Šiek tiek palaukite, kol vėl keisite aprangą',
 }
