ba56556634d8a89d6d1450a2e535a713d57f79f8
Bimbam555 <162443334+Bimbam555@users.noreply.github.com>
2024-04-01 18:36:55 +0100

    Update ak47_qb_sitchair DONE.lua

--- ak47_qb_sitchair DONE.lua
+++ ak47_qb_sitchair DONE.lua
@@ -1,11 +1,11 @@
 Locales['en'] = {
-    cmdcreatechair = 'Sukurti kėdę',
-    noaccess = "Jūs neturite leidimo tai daryti!",
-    setpos = "Pasirinkti atsisėdimą",
+    cmdcreatechair = 'Sukurti sėdimąją vietą',
+    noaccess = "Jūs neturite leidimo tai daryti",
+    setpos = "Pasirinkti animaciją",
     sit = "Atsisėsti",
     selectanim = "Stilius",
     presskey = "~INPUT_CONTEXT~ – Atsistoti\n~INPUT_VEH_HEADLIGHT~ – Stilius",
     remove = "Pašalinti",
-    removeconfirm = "Ar tikrai norite pašalinti šią kėdę?",
+    removeconfirm = "Ar tikrai norite pašalinti šią sėdimąją vietą?",
 
 }
